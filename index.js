94da59be98a1fc4578a2a2f51130bc0803e975ca
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2022-05-24 17:54:30 +0600

    car parts data load from server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
@@ -7,6 +9,32 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ng7ll.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+      await client.connect();
+      const carPartsCollection = client.db('car_parts').collection('carParts');
+      
+      app.get('/carParts', async (req, res) => {
+          const query = {};
+          const cursor = carPartsCollection.find(query);
+          const carParts = await cursor.toArray();
+          res.send(carParts);
+      })
+
+  }
+  finally{
+
+  }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('car parts')
